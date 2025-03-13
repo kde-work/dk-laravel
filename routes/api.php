b62e4129951b35ffdba41dfa13cb00aa1fa3bba5
kde-work <kde-work@mail.ru>
2025-03-13 14:32:39 +0300

    add User route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::prefix('v2')->group(function () {
-    require '../OpenAPI/Server/routes.php';
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', [UserController::class, 'show']); // Получение профиля
+        Route::put('/user', [UserController::class, 'update']); // Обновление профиля
+        Route::delete('/user', [UserController::class, 'destroy']); // Удаление пользователя
+    });
 });
