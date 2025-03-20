8d704d6ffe5b127097240832d2b115dbbb8eb4a5
kde-work <kde-work@mail.ru>
2025-03-20 16:52:07 +0300

    refactor code style

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::prefix('v2')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user', [UserController::class, 'show']); // Получение профиля
-        Route::put('/user', [UserController::class, 'update']); // Обновление профиля
-        Route::delete('/user', [UserController::class, 'destroy']); // Удаление пользователя
+        Route::get('/user', [UserController::class, 'show']);
+        Route::put('/user', [UserController::class, 'update']);
+        Route::delete('/user', [UserController::class, 'destroy']);
     });
 
     Route::post('/auth/register', [AuthController::class, 'register']);
