f069b7da1ed6021db7e9c8b39717892b3d95c416
kde-work <kde-work@mail.ru>
2025-04-02 16:31:43 +0300

    refactor minor of model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\DTO\UserDTO;
 use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
